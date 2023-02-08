0e9767c97b5bf54c85d92cd9be6541427b66f9ee
Arthur <Nazarian_2000_year@mail.ru>
2023-02-08 22:59:59 +0300

    made tabs for work

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
+/*swiper*/
+
 var swiper = new Swiper('.swiper', {
     pagination: {
         el: '.swiper-pagination',
     },
 });
+
+/*tabs*/
+
+document.querySelectorAll('.work__step').forEach(function (e) {
+
+    e.addEventListener('click', function (el) {
+
+        const tab = el.currentTarget.dataset.path;
+        document.querySelectorAll('.tab-content').forEach(function (e) {
+
+            e.classList.remove('tab-content--active')
+            document.querySelector(`[data-target='${tab}']`).classList.add('tab-content--active');
+
+
+        })
+
+    })
+
+})
