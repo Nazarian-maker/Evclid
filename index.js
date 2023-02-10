8c8406616d8a67d42893c9e7d93d7ee935c8c837
Arthur <Nazarian_2000_year@mail.ru>
2023-02-10 19:54:20 +0300

    made working burger

--- index.js
+++ index.js
@@ -49,3 +49,42 @@ $(function () {
     });
 
 });
+
+/*burger*/
+
+let burger = document.querySelector('.burger');
+let menuClose = document.querySelector('.menu-close');
+let menu = document.querySelector('.header__nav');
+let menuLinks = menu.querySelectorAll('.nav__link');
+
+burger.addEventListener('click',
+
+    function () {
+
+        menu.classList.toggle('header__nav--active');
+        document.body.classList.toggle('stop-scroll');
+
+    }
+
+)
+
+menuClose.addEventListener('click',
+
+    function () {
+
+        menu.classList.remove('header__nav--active');
+
+    }
+
+)
+
+menuLinks.forEach(function (el) {
+
+    el.addEventListener('click', function () {
+
+        menu.classList.remove('header__nav--active');
+        document.body.classList.remove('stop-scroll');
+
+    })
+
+})
